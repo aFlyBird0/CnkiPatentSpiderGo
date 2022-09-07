adff5e2d66b8d808bdbe72471f4da51a656db7ba
Bird <aflybird0@gmail.com>
2022-09-07 15:01:47 +0800

    chore: update build.sh
    Signed-off-by: Bird <aflybird0@gmail.com>

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
+# 切换至 main.go 所在目录
+cd cmd
 # 下载 gox 以一次性交叉编译所有平台
 go get github.com/mitchellh/gox
 go install github.com/mitchellh/gox
-# 切换至 main.go 所在目录
-cd cmd
+go mod tidy
 # 删除旧二进制文件
 rm -rf "../知网专利爬虫/bin/"
 # 交叉编译到所有平台, 同时忽略 darwin/386 平台，会报错
