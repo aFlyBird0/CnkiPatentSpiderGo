c6809b45dcacfab430bedcf4dd0af5196adf93d2
Bird <aflybird0@gmail.com>
2022-09-08 11:45:24 +0800

    feat: support version
    Signed-off-by: Bird <aflybird0@gmail.com>

--- build.sh
+++ build.sh
@@ -6,10 +6,12 @@ go install github.com/mitchellh/gox
 go mod tidy
 # 删除旧二进制文件
 rm -rf "../知网专利爬虫/bin/"
+# 为二进制文件加上时间版本号
+version=`date "+%Y%m%d_%H%M%S"`
 # 交叉编译到所有平台, 同时忽略 darwin/386 平台，会报错
 #gox -output "../知网专利爬虫/bin/spider_{{.OS}}_{{.Arch}}" -osarch='!darwin/386'
 # 交叉编译到几个常用的平台
-gox -output "../知网专利爬虫/bin/spider_{{.OS}}_{{.Arch}}" -osarch='darwin/amd64 linux/amd64 windows/amd64'
+gox -output "../知网专利爬虫/bin/spider_{{.OS}}_{{.Arch}}_${version}" -osarch='darwin/amd64 linux/amd64 windows/amd64'
 
 # 单独打包 MacOS 的 M1 芯片版本
-go build -o "../知网专利爬虫/bin/spider_darwin_arm64"
+go build -o "../知网专利爬虫/bin/spider_darwin_arm64_${version}"
